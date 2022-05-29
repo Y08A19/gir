d9280e507e906ef66f46813673fc83b075cab46a
unknown <khanyusuf.433@gmail.com>
2022-05-29 14:33:04 +0530

    creat

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Board from '../components/Board';
+const app = () => {
+  return (
+    <div>
+      <h1>tick tac toe</h1>
+      <Board />
+    </div>
+  );
+};
+export default app;
